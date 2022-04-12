46cc6d3fc40dd87edf2d6d76210c05b0e040dcba
Julie <julie.ribeiro.silva@gmail.com>
2022-04-12 15:28:29 -0300

    delete unused imports

--- lib/app/routes/home_routers.dart
+++ lib/app/routes/home_routers.dart
@@ -1,8 +1,4 @@
 import 'package:get/get.dart';
-import 'package:vakinha_burger/app/modules/auth/login/login_bindings.dart';
-import 'package:vakinha_burger/app/modules/auth/login/login_page.dart';
-import 'package:vakinha_burger/app/modules/auth/register/register_bindings.dart';
-import 'package:vakinha_burger/app/modules/auth/register/register_page.dart';
 import 'package:vakinha_burger/app/modules/home/home_bindings.dart';
 import 'package:vakinha_burger/app/modules/home/home_page.dart';
 
