dfa43af69088a3faa34c8f85c637c1530d26cd3e
Julie <julie.ribeiro.silva@gmail.com>
2022-04-12 15:27:30 -0300

    auth repository

--- lib/app/repositories/auth/auth_repository_impl.dart
+++ lib/app/repositories/auth/auth_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:developer';
 import 'package:vakinha_burger/app/core/exceptions/user_not_found_exception.dart';
 import 'package:vakinha_burger/app/core/restClient/rest_client.dart';
@@ -8,29 +9,22 @@ import './auth_repository.dart';
 class AuthRepositoryImpl implements AuthRepository {
   final RestClient _restClient;
 
-  AuthRepositoryImpl({
-    required RestClient restClient,
-  }) : _restClient = restClient;
+  AuthRepositoryImpl({required RestClient restClient})
+      : _restClient = restClient;
 
   @override
   Future<UserModel> register(String name, String email, String password) async {
-    final result = await _restClient.post('/auth/register', {
+    final result = await _restClient.post('/dart-week/register.php', {
       'name': name,
       'email': email,
       'password': password,
     });
-
     if (result.hasError) {
-      var message = 'Erro ao registrar usuário';
+      var message = 'Erro ao registrar usuário!';
       if (result.statusCode == 400) {
-        message = result.body['erro'];
+        message = result.body['error'];
       }
-
-      log(
-        message,
-        error: result.statusText,
-        stackTrace: StackTrace.current,
-      );
+      log(message, error: result.statusText, stackTrace: StackTrace.current);
 
       throw RestClientException(message);
     }
@@ -39,30 +33,30 @@ class AuthRepositoryImpl implements AuthRepository {
 
   @override
   Future<UserModel> login(String email, String password) async {
-    final result = await _restClient.post(
-      '/auth/',
-      {
-        'email': email,
-        'password': password,
-      },
-    );
-
+    final result = await _restClient.post('/dart-week/login.php', {
+      'email': email,
+      'password': password,
+    });
     if (result.hasError) {
       if (result.statusCode == 403) {
         log(
-          'Usuário ou senha inválidos',
+          'Usuário o senha inválidos',
           error: result.statusText,
           stackTrace: StackTrace.current,
         );
         throw UserNotFoundException();
       }
-
-      log(
-        'Erro ao autenticar o usário (${result.statusCode})',
-        error: result.statusText,
-        stackTrace: StackTrace.current,
-      );
+      log('Erro ao autenticar o usuário (${result.statusCode})',
+          error: result.statusText, stackTrace: StackTrace.current);
       throw RestClientException('Erro ao autenticar usuário');
+    }
+    if (result.statusCode == 204) {
+      return UserModel.fromMap({
+        'id': 1,
+        'name': 'Julie',
+        'email': email,
+        'password': password,
+      });
     }
     return UserModel.fromMap(result.body);
   }
