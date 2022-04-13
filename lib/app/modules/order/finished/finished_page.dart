55241355365aadec3adc1bafaaced308e65850dd
Julie <julie.ribeiro.silva@gmail.com>
2022-04-13 15:32:45 -0300

    finished page

---
+++ lib/app/modules/order/finished/finished_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:vakinha_burger/app/core/ui/widgets/vakinha_button.dart';
+
+class FinishedPage extends StatelessWidget {
+  const FinishedPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Spacer(),
+                Image.asset('assets/images/logo_rounded.png'),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                      'Pedido realizado com sucesso, clique no botão abaixo para acesso ao QRCode do pix.',
+                      textAlign: TextAlign.center,
+                      style: context.textTheme.headline6?.copyWith(
+                          color: context.theme.primaryColorDark,
+                          fontWeight: FontWeight.bold)),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: context.widthTransformer(reducedBy: 30),
+                  child: VakinhaButton(
+                    label: 'Pix',
+                    onPressed: () {},
+                    color: context.theme.primaryColorDark,
+                  ),
+                ),
+                Spacer(),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20),
+                  width: context.widthTransformer(reducedBy: 10),
+                  child: VakinhaButton(
+                    label: 'FECHAR',
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
